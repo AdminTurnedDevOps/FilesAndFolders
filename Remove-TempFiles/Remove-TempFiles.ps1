b84b44a618117a2bdd87aaeea59910d4c0cb64ca
WindowsAdmin92 <mlevan1992@gmail.com>
2017-11-12 14:40:35 -0500

    First commit for removing temp files script

---
+++ Remove-TempFiles/Remove-TempFiles.ps1
@@ -0,0 +1,56 @@
+﻿Function Remove-TempFiles {
+
+	param(
+		[string]$Temp = "C:\temp",
+
+		[string]$VariableTempDirectory = $env:TEMP,
+
+		[string]$ErrorLog = "$env:USERPROFILE\Desktop\ErrorLog.txt",
+
+		[switch]$LogError
+)
+
+
+Write-Output "Removing temp files will now begin"
+Write-Verbose "Removing temp files in the TEMP environment variable"
+
+BEGIN{}
+
+PROCESS {
+
+	TRY {
+			$ScriptWorked = $true
+			FOREACH ($Directory in $VariableTempDirectory) {
+
+				Remove-Item -Path $env:TEMP -ErrorAction SilentlyContinue
+			}
+
+Write-Verbose "Removing temo files in C:\Temp"
+
+			$MainDirectory = $Temp
+
+			FOREACH ($Directory in $MainDirectory) {
+
+				Get-childitem $Temp | Remove-Item
+			}
+   } CATCH {
+			$ScriptWorked = $false
+			Write-Output "Sorry, your script has failed. Please review the ErrorLog.txt file on your desktop"
+			IF ($LogError) {
+			$Error | Out-File $ErrorLog
+
+					
+				}
+
+			}
+}
+	
+Write-Output "Removal of temp files is now complete"
+Write-Verbose "Removed files in the TEMP environment variable and under C:\Temp"
+Write-Verbose "The C:\Temp directory still exists. All child items are removed"
+
+END{}
+
+}#FunctionClosingBracket
+
+Remove-TempFiles
