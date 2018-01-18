4af6341042c814625e5712c1c2cc8c2f828ae26a
Mike <mlevan1992@gmail.com>
2018-01-18 14:04:10 -0500

    Create New-NetShare.ps1

---
+++ New-NetShare.ps1
@@ -0,0 +1,73 @@
+Function New-NetShare {
+    [cmdletbinding(SupportsShouldProcess,ConfirmImpact='medium')]
+
+        param (
+            [ValidateNotNullOrEmpty]
+            [ValidateCount(1)]
+            [parmater(mandatory=$true)]
+            [string]$UNCSHARE,
+
+            [ValidateNotNullOrEmpty]
+            [ValidateCount(1)]
+            [parmater(mandatory=$true)]
+            [string]$DriveLetter,
+
+            [string]$ErrorLog,
+
+            [switch]$LogError
+
+)
+    
+begin{Write-Verbose "Beginning the collection of the specific UNC path and mapping"}
+
+process {
+    Write-Verbose "Starting to find the UNC path"
+    Write-Host -ForegroundColor Green "When typing in your UNC path, please do NOT put back spaces "//""
+
+    TRY {
+        Write-Verbose "Finding the UNC path. If the path is not found, prompt to retry"
+
+            $NoErrors = $true
+    do {
+
+            $path = "filesystem::\\$UNCSHARE"
+            $Testpath = Test-Path $path
+
+    
+                IF ($Testpath -match 'false') {
+
+        Write-Host "The network drive does not exist. Please try again"
+        continue
+            
+                                              }#IF Closing Bracket
+    
+                ELSEIF ($Testpath -match 'true') {
+            
+                    $PS_Drive_Params = @{
+                            'Name'=$DriveLetter
+                            'PSProvider'='FileSystem'
+                            'Root'="\\$UNCSHARE"
+                            'Persist'=$true
+                                        }
+            #cmdlet                        
+            New-PSDrive @PS_Drive_Params
+            break            
+
+                                                  }#ELSEIF Closing Bracket
+        }
+
+    while('false')
+                
+                }#TRY Closing Bracket
+
+    CATCH {
+            $NoErrors = $false
+            Write-Warning "Errors Occured. Please check the error log on your desktop"
+            IF($LogError) {
+            $Errors | Out-File $ErrorLog
+                          }
+
+                }#CATCH Closing Bracket
+            }#Process Closing Bracket
+    end{}
+}#Function Closing Bracket
