a94d0f409996b431f444afe6d4df88d589ea32eb
Mike <mlevan1992@gmail.com>
2018-02-22 16:27:40 -0500

    Update Copy-Script.ps1

--- Copy-Script.ps1
+++ Copy-Script.ps1
@@ -1,36 +1,51 @@
 $ConfirmPreference='Medium'
-Function Copy-Script {
+Function Push-Script {
     [cmdletbinding(SupportsShouldProcess=$true,ConfirmImpact='Medium')]
     param(
 
         [ValidateNotNullOrEmpty()]
-        [string[]]$ComputerName = @('PC1',
-                                    'PC2
-                                    'etc'),
+        [string[]]$ComputerName = @('NEWCCI-HYPERV02',
+                                    'NEWCCI-F1'),
                                             
         [ValidateNotNullOrEmpty()]
-        [ValidateSet('DOMAINNAME\username')]
-        [pscredential]$PSCreds= 'DOMAINNAME\USERNAME'
+        [ValidateSet('UCS\MLEVAN')]
+        [pscredential]$PSCreds= 'UCS\MLEVAN',
+
+        [string]$ErrorLog = "C:\Users\$env:USERNAME\Desktop\CopyScriptLogError.txt",
+
+        [switch]$LogError
 
     )
 
 begin { $NewSession = New-PSSession -ComputerName $ComputerName -Credential $PSCreds }
         
 process {
-            
+         
+    TRY {
+         $NoError = $true
          IF ($PSCmdlet.ShouldProcess($ComputerName)) {
                 
              FOREACH ($Computer in $NewSession) {
                                 
-                Copy-item -Path 'C:\Test\yourscript.ps1' -Destination C:\CopiedScript -ToSession $Computer 
+                Copy-item -Path 'C:\Test\mkdir.ps1' -Destination C:\CopiedScript -ToSession $Computer 
 
                 }#FOREACH
-                           
-            }#IF
 
-         }#Process
+            }#IF
+        } #TRY
+        
+    CATCH{
+            $NoError = False
+            Write-Warning "An error has occured. Please review the error logs at the specified location"
+            IF($LogError) {
+            $Error[0] | Out-File $ErrorLog
+            }
+
+        }#CATCH
+
+        }#Process
      
 end{ Get-PSSession | Remove-PSSession }         
 } #Function
 
-Copy-Script
+Push-Script
